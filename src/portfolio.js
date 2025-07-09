804ea9c5c6827681b384bc877aad8fdadc3c8736
Zerol0l <123274247+ZeroZulu@users.noreply.github.com>
2025-07-09 00:59:48 +0000

    commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -171,77 +171,77 @@ const skills = {
         "⚡ Applying design principles and user experience thinking to enhance clarity, accessibility, and impact",
       ],
       softwareSkills: [
-        {
-          skillName: "HTML5",
-          fontAwesomeClassname: "simple-icons:html5",
-          style: {
-            color: "#E34F26",
-          },
-        },
-        {
-          skillName: "CSS3",
-          fontAwesomeClassname: "fa-css3",
-          style: {
-            color: "#1572B6",
-          },
-        },
-        {
-          skillName: "Sass",
-          fontAwesomeClassname: "simple-icons:sass",
-          style: {
-            color: "#CC6699",
-          },
-        },
-        {
-          skillName: "JavaScript",
-          fontAwesomeClassname: "simple-icons:javascript",
-          style: {
-            backgroundColor: "#000000",
-            color: "#F7DF1E",
-          },
-        },
-        {
-          skillName: "ReactJS",
-          fontAwesomeClassname: "simple-icons:react",
-          style: {
-            color: "#61DAFB",
-          },
-        },
-        {
-          skillName: "NodeJS",
-          fontAwesomeClassname: "devicon-plain:nodejs-wordmark",
-          style: {
-            color: "#339933",
-          },
-        },
-        {
-          skillName: "NPM",
-          fontAwesomeClassname: "simple-icons:npm",
-          style: {
-            color: "#CB3837",
-          },
-        },
-        {
-          skillName: "Yarn",
-          fontAwesomeClassname: "simple-icons:yarn",
-          style: {
-            color: "#2C8EBB",
-          },
-        },
-        {
-          skillName: "Gatsby",
-          fontAwesomeClassname: "simple-icons:gatsby",
-          style: {
-            color: "#663399",
-          },
-        },
-        {
-          skillName: "Flutter",
-          fontAwesomeClassname: "simple-icons:flutter",
-          style: {
-            color: "#02569B",
-          },
-        },
+        //{
+        //  skillName: "HTML5",
+        //  fontAwesomeClassname: "simple-icons:html5",
+        //  style: {
+        //   color: "#E34F26",
+        //  },
+        //},
+        //{
+        //  skillName: "CSS3",
+        //  fontAwesomeClassname: "fa-css3",
+        //  style: {
+        //    color: "#1572B6",
+        //  },
+        //},
+        //{
+        //  skillName: "Sass",
+        //  fontAwesomeClassname: "simple-icons:sass",
+        //  style: {
+        //    color: "#CC6699",
+        //  },
+        //},
+        //{
+        //  skillName: "JavaScript",
+        //  fontAwesomeClassname: "simple-icons:javascript",
+        //  style: {
+        //    backgroundColor: "#000000",
+        //    color: "#F7DF1E",
+        //  },
+        //},
+        //{
+        //  skillName: "ReactJS",
+        //  fontAwesomeClassname: "simple-icons:react",
+        //  style: {
+        //    color: "#61DAFB",
+        //  },
+        //},
+        //{
+        //  skillName: "NodeJS",
+        //  fontAwesomeClassname: "devicon-plain:nodejs-wordmark",
+        //  style: {
+        //    color: "#339933",
+        //  },
+        //},
+        //{
+        //  skillName: "NPM",
+        //  fontAwesomeClassname: "simple-icons:npm",
+        //  style: {
+        //    color: "#CB3837",
+        //  },
+        //},
+        //{
+        //  skillName: "Yarn",
+        //  fontAwesomeClassname: "simple-icons:yarn",
+        //  style: {
+        //    color: "#2C8EBB",
+        //  },
+        //},
+        //{
+        //  skillName: "Gatsby",
+        // fontAwesomeClassname: "simple-icons:gatsby",
+        //  style: {
+        //    color: "#663399",
+        //  },
+        //},
+        //{
+        //  skillName: "Flutter",
+        //  fontAwesomeClassname: "simple-icons:flutter",
+        //  style: {
+        //    color: "#02569B",
+        //  },
+        //},
       ],
     },
     {
